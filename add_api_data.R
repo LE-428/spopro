75828700c4e153c74a6f263fe70defb9e0b2e39c
LE_428 <k-c.f@t-online.de>
2024-03-02 03:08:36 +0100

    updated the api function
    now including all interesting data

--- add_api_data.R
+++ add_api_data.R
@@ -1,30 +1,68 @@
 add_api_data <- function(data_table = all_data, access = access_token) {
   
-  uniq_t = unique(data_table)
-  len_uniq = length(uniq_t$ts)
   
-  uniq_t$spotify_track_uri <- substring(uniq_t$spotify_track_uri, first = 15)
-  data_table$spotify_track_uri <- substring(data_table$spotify_track_uri, first = 15)
   names(data_table)[names(data_table) == "spotify_track_uri"] <- "id"
+  data_table <- subset(data_table, select = -c(username, platform, user_agent_decrypted, episode_name, episode_show_name, spotify_episode_uri, skipped))
+  data_table$id <- substring(data_table$id, first = 15)
   
+  # uniq_t = unique(data_table)
+  len_uniq_tracks = length(data_table$ts)
+  len_uniq_artists = length(unique(data_table$master_metadata_album_artist_name))
+  uniq_artists <- aggregate(. ~ master_metadata_album_artist_name, data = data_table, FUN = function(x) x[1])
+  uniq_artists <- subset(uniq_artists, select = c(master_metadata_album_artist_name, id))
+  #return(uniq_artists)
+  for (j in seq(1, len_uniq_artists)) {
+    track <- get_track(id = uniq_artists$id[j], authorization = access_token)
+    artist_id <- track$artists$id[1]
+    uniq_artists$artist_id[j] <- artist_id
+    artist <- get_artist(id = artist_id, authorization = access_token)
+    if (length(artist$genres) == 0) {
+      uniq_artists$artist_genres[j] <- ""
+    } else {
+      uniq_artists$artist_genres[j] <- paste(artist$genres, collapse = ",")
+    }
+    uniq_artists$artist_popularity[j] <- artist$popularity
+  }
+  artist_ids <- subset(uniq_artists, select = c(artist_id, master_metadata_album_artist_name, artist_genres, artist_popularity))
   output_table <- data.frame()
   
-  for(i in seq(1, len_uniq, 100)){
-    if (i == len_uniq - (len_uniq %% 100)) {
-      index_end = len_uniq
+  for(i in seq(1, len_uniq_tracks, 100)){
+    # print(i)
+    if (i == len_uniq_tracks - (len_uniq_tracks %% 100) + 1) {
+      id_index = (len_uniq_tracks %% 100)
+      index_end = len_uniq_tracks
     } else {
+      id_index = 100
       index_end = i + 99
     }
-    
+    # print(id_index)
     # ids <- substring(all_le$spotify_track_uri[i, index_end], first = 15)
-    ids <- uniq_t$spotify_track_uri[i:index_end]
+    ids <- data_table$id[i:index_end]
+    
+    if (id_index < 51) {
+      track_table <- get_tracks(id = ids[1:id_index], authorization = access_token)
+      popularities <- track_table$popularity
+    } else {
+      track_table1 <- get_tracks(id = ids[1:50], authorization = access_token)
+      track_table2 <- get_tracks(id = ids[51:id_index], authorization = access_token)
+      popularities <- c(track_table1$popularity, track_table2$popularity)
+      # print(track_table2$popularity)
+    }
+    
     
     api_table <- get_track_audio_features(id = ids, authorization = access_token)
+    api_table <- subset(api_table, select = -c(track_href, type, uri, analysis_url))
+    api_table$popularity <- popularities
     # test_table <- get_track(ids[1], authorization = access_token)
     
-    output_table <- rbind(output_table, api_table)
+    if (nrow(output_table) == 0) {
+      output_table <- api_table
+    } else {
+      output_table <- rbind(output_table, api_table)
+    }
     
   }
-  
-  return(merge(data_table, output_table, by = "id"))
+  almighty_table <- merge(data_table, output_table, by = "id")
+  almighty_table <- merge(almighty_table, artist_ids, by = "master_metadata_album_artist_name")
+  return(almighty_table)
 }
